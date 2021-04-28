c33f5c4bd421088da49c6f88510f273b0c97fe93
Harshil pandey <harshilpandeyhld@gmail.com>
2021-04-28 09:13:12 +0530

    2nd lec

---
+++ slice.js
@@ -0,0 +1,5 @@
+let a = ["harshil","manan", "jayant", "sanchit"];
+let b = a.slice(0,3);
+console.log(b);
+let c = a.slice(-1,-4);
+console.log(c);
---
+++ split.js
@@ -0,0 +1,8 @@
+let a = "hi guys i am a robot";
+let ar = a.split(" ");
+console.log(ar);
+
+let c = "hey&i&am&Hsrshil"
+let arr = c.split("&");
+//console.log(arr);
+console.log(arr[3]);
